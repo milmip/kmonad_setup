e128ac1236c91579cc9591f821b575414d1d4e4a
Emilien Progin <emilien.progin@etik.com>
2026-04-12 14:45:47 +0200

    Ajout de localleaf

--- latex/install_depedency.sh
+++ latex/install_depedency.sh
@@ -1,6 +1,13 @@
 #bin/bash
 
+# localleaf mon fork
+
+git clone https://github.com/milmip/localleaf.git
+cd localleaf
+make install
+cd ..
+rm -rf localleaf
+
 apt update
 apt install texlive-full rofi
-
 
